50efeb0432bf335a64acdd0508a209ed389f7a39
Vadim Andreev <vadimcola@mail.ru>
2023-03-24 20:59:02 +0700

    реализованы магические методы сравнения

--- src/channel.py
+++ src/channel.py
@@ -17,7 +17,7 @@ class Channel:
         self.url = f"https://www.youtube.com/channel/{channel_id}"
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -47,6 +47,26 @@ class Channel:
     def channel_id(self):
         return self.__channel_id
 
+    def __str__(self):
+        return f"{self.title} ({self.url})"
 
-vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-# vdud.print_info()
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq_(self, other):
+        return self.subscriber_count == other.subscriber_count

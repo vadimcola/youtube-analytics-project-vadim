746de1e0f248e922e22ebf59b540b1b9d53f9a98
Vadim Andreev <vadimcola@mail.ru>
2023-04-02 17:37:02 +0700

    Реализовано исключение в инициализаторе класса Video

--- src/video.py
+++ src/video.py
@@ -8,16 +8,23 @@ class Video:
 
     def __init__(self, video_id) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id).execute()
+
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.like_count = None
+            print("IndexError")
 
     def __str__(self) -> str:
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
